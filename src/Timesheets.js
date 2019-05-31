a275f50ef3667649ff40929e9417a30f5beb7a28
Andrei Popa <andrei.deioo@gmail.com>
2019-05-31 11:37:12 +0100

    Add material-ui and tweak containers a bit

--- src/Timesheets.js
+++ src/Timesheets.js
@@ -1,12 +1,27 @@
 import React, { useState } from 'react'
+import styled, { css } from "styled-components"
+import PageContainer from "@material-ui/core/Container"
+import Paper from "@material-ui/core/Paper"
 import { Provider, Subscribe, Container } from 'unstated'
 import parseDuration from 'iso8601-duration'
+import NavBar from "./components/NavBar"
 import TimesheetsContainer from './TimesheetsContainer'
 import humanizeDuration from 'humanize-duration'
 import style from './Timesheets.module.css'
 import dayStyle from './Day.module.css'
 import dateFormat from 'dateformat'
 import ms from 'ms'
+
+const Button = styled.button`
+`
+
+const Input = styled.input`
+`
+
+const ActionsBar = styled(Paper)`
+  margin-top: 30px;
+  margin-bottom: 30px;
+`
 
 function renderDate (date) {
   return dateFormat(date, 'ddd, d mmm')
@@ -41,53 +56,58 @@ function Timesheets () {
   const [fillDuration, setFillDuration] = useState('8h')
 
   return <>
-    <h1>Timesheets</h1>
+    <NavBar elevation={3} />
     <Subscribe to={[TimesheetsContainer]}>{
       timesheets => {
         const tasks = timesheets.tasks()
 
         return <>
-          <div>
-            <button onClick={() => timesheets.fillSelected(ms(fillDuration))}>fill selected</button>
-            <input type="text" value={fillDuration} onChange={ev => setFillDuration(ev.target.value)} />
-          </div>
-          <table>
-            <thead>
-              <tr>
-                <th>date</th>
-                  {tasks.map(task => {
-                    const selectDaysForTask = ev => {
-                      timesheets.selectDaysForTask(task)
-                      ev.stopPropagation()
-                    }
+          <PageContainer maxWidth="xl">
+            <ActionsBar elevation={0}>
+              <Button onClick={() => timesheets.fillSelected(ms(fillDuration))}>fill selected</Button>
+              <Input type="text" value={fillDuration} onChange={ev => setFillDuration(ev.target.value)} />
+            </ActionsBar>
 
-                    return <th onClick={selectDaysForTask}>{task.name}</th>
-                  })}
-              </tr>
-            </thead>
-            <tbody>{
-              timesheets.days().map(day => {
-                const className = dayStyle[day.type]
-                return <tr className={className}>
-                  <td>{renderDate(day.date)}</td>
-                  {
-                    tasks.map(task => {
-                      const dayTask = day.tasks[task.id]
-
-                      const className = s => [dayTask && dayTask.selected ? style.selected : '', s].filter(Boolean).join(' ')
-
-                      if (dayTask.duration) {
-                        return <td onClick={() => timesheets.toggleSelection(dayTask)} className={className(style.duration)}>{renderDuration(dayTask.duration)}</td>
-                      } else {
-                        return <td onClick={() => timesheets.toggleSelection(dayTask)} className={className(style.noDuration)}></td>
+            <Paper>
+            <table style={{width: '100%'}}>
+              <thead>
+                <tr>
+                  <th>date</th>
+                    {tasks.map(task => {
+                      const selectDaysForTask = ev => {
+                        timesheets.selectDaysForTask(task)
+                        ev.stopPropagation()
                       }
-                    })
-                  }
-                  <td>{day.comments.join()}</td>
+
+                      return <th onClick={selectDaysForTask}>{task.name}</th>
+                    })}
                 </tr>
-              })
-            }</tbody>
-          </table>
+              </thead>
+              <tbody>{
+                timesheets.days().map(day => {
+                  const className = dayStyle[day.type]
+                  return <tr className={className}>
+                    <td>{renderDate(day.date)}</td>
+                    {
+                      tasks.map(task => {
+                        const dayTask = day.tasks[task.id]
+
+                        const className = s => [dayTask && dayTask.selected ? style.selected : '', s].filter(Boolean).join(' ')
+
+                        if (dayTask.duration) {
+                          return <td onClick={() => timesheets.toggleSelection(dayTask)} className={className(style.duration)}>{renderDuration(dayTask.duration)}</td>
+                        } else {
+                          return <td onClick={() => timesheets.toggleSelection(dayTask)} className={className(style.noDuration)}></td>
+                        }
+                      })
+                    }
+                    <td>{day.comments.join()}</td>
+                  </tr>
+                })
+              }</tbody>
+            </table>
+            </Paper>
+          </PageContainer>  
         </>
       }
     }</Subscribe>
---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react"
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+const NavBar = () => {
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography component="h2" variant="title" color="inherit">
+            Timesheets
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default NavBar
